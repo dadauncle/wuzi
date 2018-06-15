e2b122f05b8fb03e397125e13c8ce73a23efd083
Memory789 <948581562@qq.com>
2018-06-15 12:19:05 +0800

    xiaoyu

--- enter/js/outBoundDetail.js
+++ enter/js/outBoundDetail.js
@@ -4,15 +4,36 @@ window.onload = function() {
 		autoHeight: true,
 		slidesPerView: 4,
 	})
-	
-	$('input,textarea').attr('readonly', 'readonly').css('outline','none')
+
+	$('input,textarea').attr('readonly', 'readonly').css('outline', 'none')
+
+	//获取详情
+	getInfo()
+	function getInfo() {
+		$.ajax({
+			type: "get",
+			url: apiHost + "/appOutStorage/findById",
+			data: {
+				'uid': userId,
+				"outStorageId":"1"
+			},
+			async: true,
+			success: function(data) {
+				console.log(data);
+				//渲染仓库
+				var info = data.data;
+				renderInfo(info)
+			},
+			error: function(data) {
+				console.log(data)
+			}
+		});
+	}
 };
 $(".row").css({
 	border: "none",
 	padding: "0rem"
 })
-
-
 
 window.onresize = function() {
 	getRem(750, 100)
